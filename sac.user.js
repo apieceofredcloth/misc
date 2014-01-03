3b05c1f9cab0542d2e354caa717b32bd1ed8fece
yushengjun <apieceofredcloth@gmail.com>
2014-01-03 12:02:35 +0800

    fix bug;bind click event to button;

--- sac.user.js
+++ sac.user.js
@@ -7,7 +7,7 @@
 // @include        http://www.steepandcheap.com/gear-cache/*
 // ==/UserScript==
 
-var price_rxp = /[0-9\.]+/g;
+var float_rxp = /[0-9\.]+/g;
 var $reset = document.querySelector('.collection-facets .reset');
 
 function trim(str) {
@@ -15,16 +15,17 @@ function trim(str) {
 }
 
 function get_discount(ele) {
-    return parseInt(ele.querySelector('.product-discount').innerText)
+    var discount = ele.querySelector('.product-discount').innerText;
+    return parseFloat(discount.match(float_rxp)[0])
 }
 
 function cmp_discount(a, b) {
-    return get_discount(a) - get_discount(b);
+    return get_discount(b) - get_discount(a);
 }
 
 function get_price(ele) {
     var price = ele.querySelector('.product-price').innerText;
-    return parseFloat(price.match(price_rxp)[0])
+    return parseFloat(price.match(float_rxp)[0])
 }
 
 function cmp_price(a, b) {
@@ -34,6 +35,7 @@ function cmp_price(a, b) {
 function sort(sort_type) {
     var goods=[];
     var nodes = document.querySelectorAll('.collection-product:not(.ng-hide)');
+    var $list = document.querySelector('.collection-products');
     for(var i=0; i<nodes.length; i++){
         goods.push(nodes[i]);
     }
@@ -43,7 +45,7 @@ function sort(sort_type) {
         goods.sort(cmp_price);
     }
     for(var i=0; i<goods.length; i++){
-        document.querySelector('.collection-products').appendChild(goods[i]);
+        $list.appendChild(goods[i]);
     }
 }
 
@@ -58,11 +60,14 @@ function init() {
         '<button>discount</button><button>price</button>'+
         '</div>';
     var $facet = parseDom(sort_ele_str)[0];
-    $facet.addEventListener('click', function() {
-        var sort_type = trim(this.innerText);
-        sort(sort_type);
-    }, false);
-    $reset.parentNode.insertBefore($facet, $reset)
+    var $buttons = $facet.querySelectorAll('button');
+    for(var i=0; i<$buttons.length; i++){
+        $buttons[i].addEventListener('click', function() {
+            var sort_type = trim(this.innerText);
+            sort(sort_type);
+        }, false);
+        $reset.parentNode.insertBefore($buttons[i], $reset)
+    }
 }
 
 init();
